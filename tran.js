c417a97a165d556237b0f5eac8acf0dcf025c1f5
Alex <yoyobq@hotmail.com>
2023-03-27 14:43:52 +0000

    feat: 删除本服务器上存储的 API_KEY，设置服务器错误处理流程，为两种不同的 mode 设置分支

--- tran.js
+++ tran.js
@@ -1,21 +1,38 @@
 import http from 'http';
 import { Configuration, OpenAIApi } from "openai";
 
-const API_KEY = '';
-
 const server = http.createServer((req, res) => {
   let body = '';
   req.on('data', chunk => {
     body += chunk;
   });
   req.on('end', async () => {
-    console.log(body);
-    const question = JSON.parse(body);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    const answer = await askQuestion(question);
-    console.log(answer);
-    res.end(JSON.stringify(answer));
+    try {
+      const data = JSON.parse(body);
+      // console.log(data);
+      const question = data.query;
+      const API_KEY = data.API_KEY;
+      const mode = data.mode;
+      // console.log(mode);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      
+      let answer;
+      if (mode === 'complete') {
+        answer = await GeneratingText(question, API_KEY);
+      } else {
+        if (mode === 'chat') {
+          answer = await talk(question, API_KEY);
+        }
+      }
+
+      res.end(JSON.stringify(answer));
+    } catch (error) {
+      console.error(error);
+      res.statusCode = 500;
+      res.end('Internal Server Error');
+    }
+  
   });
 });
 
@@ -23,26 +40,35 @@ server.listen(8080, () => {
   console.log('Server started on port 8080');
 });
 
-// 处理提交内容
+async function talk(question, API_KEY) {
+  console.log(question);
+  return 0;
+}
 
-
-// 向 Chat GPT 提交问题，并获得相应
-const configuration = new Configuration({
-  apiKey: API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
-async function askQuestion(question) {
+// 对应 comoplete mode
+async function GeneratingText(question, API_KEY) {
   try {
+    // 创建 OpenAIapi 实例，向 Chat GPT 提交问题
+    const configuration = new Configuration({
+      apiKey: API_KEY,
+    });
+    let openai = new OpenAIApi(configuration);
+    
+    console.log(question);
+    // 获得响应
     const completion = await openai.createCompletion(question);
-    return { result: completion.data.choices[0].text };
-    // console.log(completion.data.choices[0].text);
-    // return completion;
+
+    // 关闭实例
+    openai = null;
+    // console.log(completion.data);
+    // return { result: completion.data.choices[0].text };
+    console.log(completion.data.choices[0].text);
+    return completion.data;
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
+      // res.status(error.response.status).json(error.response.data);
     } else {
       console.error(`Error with OpenAI API request: ${error.message}`);
       res.status(500).json({
